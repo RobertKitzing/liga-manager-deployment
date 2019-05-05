177356ec4e0d304fe066dcf43c3ae30d1a3f5028
Marius Klocke <marius.klocke@gmail.com>
2019-05-05 12:38:49 +0000

    Simplify cert renewal

--- renew-cert.sh
+++ renew-cert.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker-compose stop nginx
-docker run --rm -p 80:80 -p 443:443 -v $PWD/volumes/letsencrypt:/etc/letsencrypt certbot/certbot renew --standalone
-docker-compose start nginx
+docker-compose exec nginx sh -c "apk add certbot certbot-nginx"
+docker-compose exec nginx sh -c "certbot --nginx renew"
+
