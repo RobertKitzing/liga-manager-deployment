27fd74981e08f3c402785b89d45ef996db60ab13
Marius Klocke <marius.klocke@gmail.com>
2022-05-13 19:45:43 +0200

    Simplify copy-data.sh

--- migration/copy-data.sh
+++ migration/copy-data.sh
@@ -3,20 +3,20 @@
 set -ex
 
 SSH_ARGS="-i /root/.ssh/migration_key root@157.230.106.170"
-TEMP_FILE="$(mktemp)"
 
 # Determine latest backups
-ssh $SSH_ARGS sh -s > "$TEMP_FILE" <<EOF
-  cd /opt/liga-manager
-  ls -t backup/wp-files*.tar.gz | head -n 1
-  ls -t backup/wordpress*.sql.gz | head -n 1
-  ls -t backup/api*.sql.gz | head -n 1
+BACKUP_FILES=$(ssh $SSH_ARGS sh -s <<EOF
+  find /opt/liga-manager/backup -type f -name 'api*.sql.gz' | sort -r | head -n 1
+  find /opt/liga-manager/backup -type f -name 'wordpress*.sql.gz' | sort -r | head -n 1
+  find /opt/liga-manager/backup -type f -name 'wp-files*.tar.gz' | sort -r | head -n 1
 EOF
+)
 
 # Transfer latest backups
-while read -r BACKUP_FILE; do
-  scp $SSH_ARGS:"/opt/liga-manager/$BACKUP_FILE" /tmp/
-done < "$TEMP_FILE"
+for BACKUP_FILE in $BACKUP_FILES
+do
+  scp $SSH_ARGS:$BACKUP_FILE /tmp/
+done
 
 # Restore backups on target host
 zcat /tmp/api*.sql.gz | docker compose exec -T mariadb mysql lima
@@ -24,4 +24,4 @@ zcat /tmp/wordpress*.sql.gz | docker compose exec -T mariadb mysql wordpress
 tar xfz /tmp/wp-files*.tar.gz -C /var/lib/docker/volumes/liga-manager_wp-files/_data --strip-components 2
 
 # Cleanup
-rm /tmp/api*.sql.gz /tmp/wordpress*.sql.gz /tmp/wp-files*.tar.gz "$TEMP_FILE"
+rm /tmp/api*.sql.gz /tmp/wordpress*.sql.gz /tmp/wp-files*.tar.gz
